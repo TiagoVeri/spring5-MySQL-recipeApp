312d6d7bc41296daa712421117c77195911d3eaa
TiagoVeri <tiagoverisjc@gmail.com>
2019-04-24 15:24:01 -0300

    added methods to retrieve some information from Database with Spring Data JPA Query Methods

--- src/main/java/guru/assignment/recipe/controllers/IndexController.java
+++ src/main/java/guru/assignment/recipe/controllers/IndexController.java
@@ -1,13 +1,37 @@
 package guru.assignment.recipe.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.assignment.recipe.domain.Category;
+import guru.assignment.recipe.domain.UnitOfMeasure;
+import guru.assignment.recipe.repositories.CategoryRepository;
+import guru.assignment.recipe.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class IndexController {
 
+	private CategoryRepository categoryRepository;
+	
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+	
+	
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
+
 	@RequestMapping({"", "/", "/index"})
 	public String getIndexPage() {
+		
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+		System.out.println("Category Id is: " + categoryOptional.get().getId());
+		System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
 		return "index";
 	}
 }
--- src/main/java/guru/assignment/recipe/repositories/CategoryRepository.java
+++ src/main/java/guru/assignment/recipe/repositories/CategoryRepository.java
@@ -1,9 +1,12 @@
 package guru.assignment.recipe.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import guru.assignment.recipe.domain.Category;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
+	Optional<Category> findByDescription(String description);
 }
