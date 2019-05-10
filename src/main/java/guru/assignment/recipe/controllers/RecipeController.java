919e4bff0e9fb524dd2264317893475543f91763
TiagoVeri <tiagoverisjc@gmail.com>
2019-05-10 20:51:18 -0300

    Added recipeform view page and some controller logic to use it

--- src/main/java/guru/assignment/recipe/controllers/RecipeController.java
+++ src/main/java/guru/assignment/recipe/controllers/RecipeController.java
@@ -2,9 +2,12 @@ package guru.assignment.recipe.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import guru.assignment.recipe.commands.RecipeCommand;
 import guru.assignment.recipe.service.RecipeService;
 
 @Controller
@@ -22,6 +25,20 @@ public class RecipeController {
 		model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 		
 		return "recipe/show";
+	}
+	
+	@RequestMapping("recipe/new")
+	public String newRecipe(Model model) {
+		model.addAttribute("recipe", new RecipeCommand());
+		
+		return "recipe/recipeform";
+	}
+	
+	@PostMapping("recipe")
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+		
+		return"redirect:/recipe/show/" + savedCommand.getId();
 	}
 
 }
