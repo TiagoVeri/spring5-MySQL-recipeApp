47a1138d12971a1de7cfe145b51e6b25e462cd9a
TiagoVeri <tiagoverisjc@gmail.com>
2019-05-15 21:47:49 -0300

    added Image upload option, view, controller and tests

---
+++ src/main/java/guru/assignment/recipe/service/ImageService.java
@@ -0,0 +1,8 @@
+package guru.assignment.recipe.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+	void saveImageFile(Long recipeId, MultipartFile file);
+}
