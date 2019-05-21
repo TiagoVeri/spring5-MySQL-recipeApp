14a16aa9e6bfdb7e009d7f2e822fda59306be1e4
TiagoVeri <tiagoverisjc@gmail.com>
2019-05-21 11:12:56 -0300

    Added some Data Validations and test for it

--- src/main/java/guru/assignment/recipe/commands/RecipeCommand.java
+++ src/main/java/guru/assignment/recipe/commands/RecipeCommand.java
@@ -3,23 +3,14 @@ package guru.assignment.recipe.commands;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.Lob;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
-import guru.assignment.recipe.domain.Category;
+import org.hibernate.validator.constraints.URL;
+
 import guru.assignment.recipe.domain.Difficulty;
-import guru.assignment.recipe.domain.Ingredient;
-import guru.assignment.recipe.domain.Notes;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -29,13 +20,30 @@ import lombok.Setter;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+    
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
+    
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+    
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+    
+    @Min(1)
+    @Max(100)
     private Integer servings;
     private String source;
+    
+    @URL
     private String url;
+    
+    @NotBlank
     private String directions;
+    
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Byte[] image;
     private Difficulty difficulty;
